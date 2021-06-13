10dedb4b31f8d29ef794c2ee6024ad61222b89dd
dmitryame <dmitry@custommode.com>
2021-06-12 22:49:28 -0400

    better structuring module

--- src/index.js
+++ src/index.js
@@ -78,85 +78,88 @@ const _makeSureDirectoryExists = async ({ directory }) => {
   }
 }
 
-export const addToCache = async ({ file, key }) => {
-  await FileSystem.copyAsync({
-    from: file,
-    to: `${CONST.IMAGE_CACHE_FOLDER}${key}`,
-  })
-  // const uri = await FileSystem.getContentUriAsync(`${CONST.IMAGE_CACHE_FOLDER}${key}`)
-  // return uri
-  return `${CONST.IMAGE_CACHE_FOLDER}${key}`
-}
+export const CacheManager = {
+  addToCache: async ({ file, key }) => {
+    await FileSystem.copyAsync({
+      from: file,
+      to: `${CONST.IMAGE_CACHE_FOLDER}${key}`,
+    })
+    // const uri = await FileSystem.getContentUriAsync(`${CONST.IMAGE_CACHE_FOLDER}${key}`)
+    // return uri
+    return `${CONST.IMAGE_CACHE_FOLDER}${key}`
+  },
 
-export const initCache = async () => {
-  await _makeSureDirectoryExists({ directory: CONST.IMAGE_CACHE_FOLDER })
-  // alert('cache folder exists')
+  initCache: async () => {
+    await _makeSureDirectoryExists({ directory: CONST.IMAGE_CACHE_FOLDER })
+    // alert('cache folder exists')
 
-  // const cachedFiles = await FileSystem.readDirectoryAsync(`${CONST.IMAGE_CACHE_FOLDER}`)
-  //
-  // const cachedUri = `${CONST.IMAGE_CACHE_FOLDER}${cachedFiles[0]}`
-  // console.log({ length: cachedFiles.length })
-  // console.log({ cachedUri })
-  //
-  // for (let i = 0; i < 20000; i += 1) {
-  //   // eslint-disable-next-line no-await-in-loop
-  //   await FileSystem.copyAsync({
-  //     from: cachedUri,
-  //     to: `${cachedUri}_${i}`,
-  //   })
-  // }
-  // console.log('-------------------------------------------DONE-----------------------------------')
+    // const cachedFiles = await FileSystem.readDirectoryAsync(`${CONST.IMAGE_CACHE_FOLDER}`)
+    //
+    // const cachedUri = `${CONST.IMAGE_CACHE_FOLDER}${cachedFiles[0]}`
+    // console.log({ length: cachedFiles.length })
+    // console.log({ cachedUri })
+    //
+    // for (let i = 0; i < 20000; i += 1) {
+    //   // eslint-disable-next-line no-await-in-loop
+    //   await FileSystem.copyAsync({
+    //     from: cachedUri,
+    //     to: `${cachedUri}_${i}`,
+    //   })
+    // }
+    // console.log('-------------------------------------------DONE-----------------------------------')
 
-  // if (Platform.OS === 'ios') {
-  // cleanup old cached files
-  const cachedFiles = await FileSystem.readDirectoryAsync(`${CONST.IMAGE_CACHE_FOLDER}`)
+    // if (Platform.OS === 'ios') {
+    // cleanup old cached files
+    const cachedFiles = await FileSystem.readDirectoryAsync(`${CONST.IMAGE_CACHE_FOLDER}`)
 
-  let position = 0
-  let results = []
-  const batchSize = 20
+    let position = 0
+    let results = []
+    const batchSize = 20
 
-  // batching promise.all to avoid exxessive promisses call
-  while (position < cachedFiles.length) {
-    const itemsForBatch = cachedFiles.slice(position, position + batchSize)
+    // batching promise.all to avoid exxessive promisses call
+    while (position < cachedFiles.length) {
+      const itemsForBatch = cachedFiles.slice(position, position + batchSize)
         results = [...results, ...await Promise.all(itemsForBatch.map(async file => {// eslint-disable-line
           const info = await FileSystem.getInfoAsync(`${CONST.IMAGE_CACHE_FOLDER}${file}`)// eslint-disable-line
-      return Promise.resolve({ file, modificationTime: info.modificationTime, size: info.size })
-    }))]
-    position += batchSize
-  }
-
-  // cleanup cache, leave only 500mb wirth of most recent files
-  results
-    .sort((a, b) => a.modificationTime - b.modificationTime)
-
-  let sumSize = results.reduce((accumulator, currentValue) => accumulator + Number(currentValue.size), 0)
-
-  // let's calculate the sum in the first pass
-  // second pass to clean up the cach files based on the total size of files in the cache
-  for (let i = 0; i < results.length; i += 1) {
-    if (sumSize > 400 * 1000 * 1000) { // 0.4GB
-      FileSystem.deleteAsync(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`, { idempotent: true })
-      sumSize -= results[i].size
+        return Promise.resolve({ file, modificationTime: info.modificationTime, size: info.size })
+      }))]
+      position += batchSize
     }
-  }
-  // alert(`sumSize:${sumSize} cachedFiles:${cachedFiles.length}`)
 
-  // alert(`sorted.length ${sorted.length}`)
-  // second pass to clean up the cach files based on the total number of files in the cache
+    // cleanup cache, leave only 500mb wirth of most recent files
+    results
+      .sort((a, b) => a.modificationTime - b.modificationTime)
 
-  // for (let i = 0; (results.length - i) > 1000; i += 1) { // may need to reduce down to 500
-  //   // console.log(sorted[i].modificationTime)
-  //   if (i === 0) {
-  //     alert(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`)
-  //   }
-  //   await FileSystem.deleteAsync(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`, { idempotent: true }) // eslint-disable-line
-  // }
+    let sumSize = results.reduce((accumulator, currentValue) => accumulator + Number(currentValue.size), 0)
+
+    // let's calculate the sum in the first pass
+    // second pass to clean up the cach files based on the total size of files in the cache
+    for (let i = 0; i < results.length; i += 1) {
+      if (sumSize > 400 * 1000 * 1000) { // 0.4GB
+        FileSystem.deleteAsync(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`, { idempotent: true })
+        sumSize -= results[i].size
+      }
+    }
+    // alert(`sumSize:${sumSize} cachedFiles:${cachedFiles.length}`)
+
+    // alert(`sorted.length ${sorted.length}`)
+    // second pass to clean up the cach files based on the total number of files in the cache
+
+    // for (let i = 0; (results.length - i) > 1000; i += 1) { // may need to reduce down to 500
+    //   // console.log(sorted[i].modificationTime)
+    //   if (i === 0) {
+    //     alert(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`)
+    //   }
+    //   await FileSystem.deleteAsync(`${CONST.IMAGE_CACHE_FOLDER}${results[i].file}`, { idempotent: true }) // eslint-disable-line
+    // }
 
   // console.log('----------------------------')
   // console.log({ sumSize })
   // console.log({ cachedFilesCount })
   // console.log('----------------------------')
   // }
+  },
+
 }
 
 export default CachedImage
