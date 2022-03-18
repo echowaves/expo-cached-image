23aa6fec43d44d0a9fae6b3d85e90add7b4d6f6e
dmitryame <dmitry@custommode.com>
2022-03-18 10:04:02 -0400

    updating .d.ts

--- .d.ts
+++ .d.ts
@@ -1,1 +1,14 @@
-declare module 'expo-cached-image';
+declare module 'expo-cached-image' {
+    import { ImageSourcePropType, ImageStyle, StyleProp } from 'react-native';
+    
+    interface CachedImagesProps {
+        source?: ImageSourcePropType;
+        style: StyleProp<ImageStyle>;
+        cacheKey: string;
+        resizeMode: string;
+    }
+    
+    class CachedImages extends React.Component<CachedImagesProps> {}
+    export default CachedImages;
+    }
+    
