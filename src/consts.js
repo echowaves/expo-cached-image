995d2a7ec7333d709cfb2be5a45c4794b64e90d5
dmitryame <dmitry@custommode.com>
2023-03-01 20:00:46 -0500

    removed the type declaration

--- src/consts.js
+++ src/consts.js
@@ -1,4 +1,4 @@
-import * as FileSystem from 'expo-file-system'
+import * as FileSystem from "expo-file-system"
 
 export const IMAGE_CACHE_FOLDER = `${FileSystem.cacheDirectory}`
 // export const IMAGE_CACHE_FOLDER = `${FileSystem.documentDirectory}images/`
